e0646b25f9c18fe7838b08d532b5dfb77f9ec904
Harvey <laozhaochaguan@gmail.com>
2019-11-21 16:36:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 export default class AudioImage {
   constructor(option = {}) {
+    this.option = {
+      ...AudioImage.option,
+      ...option,
+    };
+  }
+
+  static get option() {
+    return {
+      baseImage: '',
+      encodeImage: '',
+      recorder: { audioBitsPerSecond: 128000, mimeType: 'audio/webm' },
+    };
+  }
+
+  record() {
     //
+  }
+
+  stop() {
+    //
+  }
+
+  load(buf) {
+    //
+  }
+
+  play() {
+    //
+  }
+
+  encode(buf) {
+    //
+  }
+
+  decode(img) {
+    //
+  }
+
+  getAudioUrl() {
+    return URL.createObjectURL();
   }
 }
